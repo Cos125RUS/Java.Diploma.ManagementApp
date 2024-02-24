dc864e4ad8238680625fefd2e435bb387d5561ca
Valerii_Panov <val250640@mail.ru>
2024-02-24 11:03:52 +1000

    fix eureka bag

--- gateway/src/main/java/ru/steeldv/gateway/GatewayApplication.java
+++ gateway/src/main/java/ru/steeldv/gateway/GatewayApplication.java
@@ -2,6 +2,9 @@ package ru.steeldv.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayApplication {
@@ -10,4 +13,11 @@ public class GatewayApplication {
 		SpringApplication.run(GatewayApplication.class, args);
 	}
 
+	@Bean
+	public RouteLocator managementAppLocator(RouteLocatorBuilder builder) {
+		return builder.routes()
+//				.route("site", r -> r.path("/site/**").uri("http://localhost:8080/"))
+//				.route("notes", r -> r.path("/notes/**").uri("http://localhost:8081/"))
+				.build();
+	}
 }
