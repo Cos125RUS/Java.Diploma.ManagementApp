eef445fd83cbd006361cf4f20585e2a4711c6ccb
Valerii_Panov <val250640@mail.ru>
2024-03-05 15:05:12 +1000

    add classes to catalog in page

--- front/src/main/resources/static/js/catalog.js
+++ front/src/main/resources/static/js/catalog.js
@@ -8,11 +8,11 @@
 
 let categories = null;
 const units = {
-    kilogram: "кг", 
-    ton: "т", 
+    kilogram: "кг",
+    ton: "т",
     unit: "шт"
 };
-window.addEventListener('load', function (e) {
+document.addEventListener('DOMContentLoaded', function (e) {
     const url = "/api/catalog/category/findAll";
     const req = new XMLHttpRequest();
     req.open("GET", url);
@@ -21,29 +21,37 @@ window.addEventListener('load', function (e) {
         try {
             categories = JSON.parse(req.responseText);
         } catch (error) {
-            // console.log(error);
+            console.log(error);
         }
     }
+
 });
 
-const createUl = (parentElement, list) => {
+const createUl = (parentElement, list, className, hidden) => {
     const ulElement = document.createElement('ul');
     parentElement.appendChild(ulElement);
+    ulElement.classList.add(className);
+    if (hidden) {
+        ulElement.classList.add(hidden);
+    }
     list.forEach(element => {
-        ulElement.appendChild(createLi(element));
+        ulElement.appendChild(createLi(element, className));
     });
 };
 
-const createLi = (element) => {
+const createLi = (element, className) => {
     const liElement = document.createElement('li');
+    liElement.classList.add(className + '_' + element.id);
     const pName = document.createElement('p');
     liElement.appendChild(pName);
     if (Object.keys(element).indexOf('name') !== -1) {
         pName.textContent = element.name;
-        Array.from(Object.values(element)).forEach(values => {
-            if (Array.isArray(values)) {
-                createUl(liElement, values);
+        let counter = 0;
+        Object.values(element).forEach(value => {
+            if (Array.isArray(value)) {
+                createUl(liElement, value, Object.keys(element)[counter] + '_' + element.id, 'hidden');
             }
+            counter++;
         });
     } else if (Object.keys(element).indexOf('print') !== -1) {
         pName.textContent = element.print + ' | ' + element.weight + ' | ' + element.unitPrice + 'р/' + units[element.baseUnitType];
@@ -53,7 +61,7 @@ const createLi = (element) => {
 
 const catalogFilling = () => {
     const contentDiv = document.querySelector('.content');
-    createUl(contentDiv, categories);
+    createUl(contentDiv, categories, "category", false);
 };
 
 const wait = () => {
@@ -64,4 +72,4 @@ const wait = () => {
     }
 };
 
-wait();
+window.addEventListener('load', wait);
