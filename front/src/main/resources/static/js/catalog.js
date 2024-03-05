71c20ed3abc0f108fe828a176bca3b96b07c4c14
Valerii_Panov <val250640@mail.ru>
2024-03-05 19:32:31 +1000

    add current element view

--- front/src/main/resources/static/js/catalog.js
+++ front/src/main/resources/static/js/catalog.js
@@ -1,17 +1,55 @@
+// #region thymeleaf
 // let categories = null;
 // setTimeout(() => {
 //     const data = document.querySelector("script.categories-data")
 //     console.log(data);
 //     // categories = JSON.parse(data.dataset.categories);
 //     // console.log(categories);
-// }, 1000)
+// }, 100)
+// #endregion
 
-let categories = null;
+// #region models
 const units = {
     kilogram: "кг",
     ton: "т",
     unit: "шт"
 };
+
+const currentElement = {
+    category: {
+        name: null,
+        id: null
+    },
+    subcategories: {
+        name: null,
+        id: null
+    },
+    products: {
+        name: null,
+        id: null
+    },
+    items: {
+        name: null,
+        id: null
+    }
+};
+// #endregion
+
+// #region userInterface
+let currentElementDiv = null;
+let currentElementView = null;
+
+document.addEventListener('DOMContentLoaded', function (e) {
+    currentElementDiv = document.querySelector('div.user-interface>div.user-interface__current-element');
+    currentElementView = document.createElement('p');
+    currentElementDiv.appendChild(currentElementView);
+});
+
+// #endregion
+
+// #region catalogList
+let categories = null;
+
 document.addEventListener('DOMContentLoaded', function (e) {
     const url = "/api/catalog/category/findAll";
     const req = new XMLHttpRequest();
@@ -24,7 +62,6 @@ document.addEventListener('DOMContentLoaded', function (e) {
             console.log(error);
         }
     }
-
 });
 
 const createUl = (parentElement, list, className, hidden) => {
@@ -36,11 +73,25 @@ const createUl = (parentElement, list, className, hidden) => {
         try {
             if (e.target.nextElementSibling.classList.contains('hidden')) {
                 e.target.nextElementSibling.classList.remove('hidden');
+                currentElement[e.target.dataset.catalogType].id = e.target.dataset.id;
+                currentElement[e.target.dataset.catalogType].name = e.target.innerHTML;
+                console.log(e.target.dataset.catalogType);
+                switch (e.target.dataset.catalogType) {
+                    case 'category':
+                        currentElementView.innerHTML = currentElement.category.name;
+                        break;
+                    case 'subcategories':
+                        currentElementView.innerHTML += ">" + currentElement.subcategories.name;
+                        break;
+                    case 'products':
+                        currentElementView.innerHTML += ">" + currentElement.products.name;
+                        break;
+                }
             } else {
                 e.target.nextElementSibling.classList.add('hidden');
             }
         } catch (error) {
-            console.log(error);            
+            console.log(error);
         }
     });
     if (hidden) {
@@ -53,15 +104,19 @@ const createUl = (parentElement, list, className, hidden) => {
 
 const createLi = (element, className) => {
     const liElement = document.createElement('li');
-    liElement.classList.add(className + '_' + element.id);
+    // liElement.classList.add(className + '_' + element.id);
+    liElement.classList.add(className);
     const pName = document.createElement('p');
     liElement.appendChild(pName);
     if (Object.keys(element).indexOf('name') !== -1) {
         pName.textContent = element.name;
+        pName.dataset.id = element.id;
+        pName.dataset.catalogType = className;
         let counter = 0;
         Object.values(element).forEach(value => {
             if (Array.isArray(value)) {
-                createUl(liElement, value, Object.keys(element)[counter] + '_' + element.id, 'hidden');
+                // createUl(liElement, value, Object.keys(element)[counter] + '_' + element.id, 'hidden');
+                createUl(liElement, value, Object.keys(element)[counter], 'hidden');
             }
             counter++;
         });
@@ -85,3 +140,5 @@ const wait = () => {
 };
 
 window.addEventListener('load', wait);
+// #endregion
+
