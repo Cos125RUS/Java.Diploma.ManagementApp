4b1b4f07f9d015f696f7789a9ab3b82fdff268ca
Valerii_Panov <val250640@mail.ru>
2024-03-09 16:31:20 +1000

    add load clients page

---
+++ front/src/main/resources/static/js/clients.js
@@ -0,0 +1,30 @@
+let clients = null;
+
+document.addEventListener('DOMContentLoaded', function (e) {
+    const url = "/api/clients/findAll";
+    const req = new XMLHttpRequest();
+    req.open("GET", url);
+    req.send();
+    req.onreadystatechange = (e) => {
+        try {
+            clients = JSON.parse(req.responseText);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+});
+
+const clientsPageFilling = () => {
+    const contentDiv = document.querySelector('.content');
+    console.log(clients);
+};
+
+const wait = () => {
+    if (clients === null) {
+        setTimeout(wait, 100);
+    } else {
+        clientsPageFilling();
+    }
+};
+
+window.addEventListener('load', wait);
